7dea524fb3a3e444166ac18aad05659392f374c7
sasha radkova <sasharadkova@outlook.com>
2022-06-29 15:26:39 +0200

    filled IssueController with a HttpGet Action Method

--- APItest/Controllers/IssueController.cs
+++ APItest/Controllers/IssueController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APItest.Data;
+using APItest.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APItest.Controllers
 {
@@ -10,5 +13,17 @@ namespace APItest.Controllers
     // the ControllerBase class provides usefull properties and methodes to help you manage HTTP requests
     public class IssueController : ControllerBase
     {
+        // we declate the DbContext in the constructor so that the controller can access the db, this way we
+        // will get an instance of it at the runtime
+
+        private readonly IssueDbContext _context;
+        public IssueController(IssueDbContext context) => _context = context;  
+
+        // an Action Method is a public method that is executed in response to an HTTP request
+        // we create an Action Methid that will be mapped to the HTTP verb GET, in oreder to get a list of Issues
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get() // Look this up! 
+        
+            => await _context.Issues.ToListAsync();      
     }
 }
