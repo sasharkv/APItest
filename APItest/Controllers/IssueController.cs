6171d6c091ef2c2a09f8b9f04a446c2fb8845b4b
sasha radkova <sasharadkova@outlook.com>
2022-06-29 14:59:07 +0200

    added controller - NB: make sure to pick API controller from the menu, not MVC

---
+++ APItest/Controllers/IssueController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APItest.Controllers
+{
+    [Route("api/[controller]")] // this means the url will be api/issue, so the controller name without the "controller" suffix
+    [ApiController] // this attribute is used to apply common conventions to your controller: like automatic validation of the model, binding
+    // request data to the model etc
+
+    // the ControllerBase class provides usefull properties and methodes to help you manage HTTP requests
+    public class IssueController : ControllerBase
+    {
+    }
+}
