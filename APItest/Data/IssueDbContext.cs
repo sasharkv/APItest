d4a5f6fbcf7eeafc3a1c12f8f363a510ac854fad
sasha radkova <sasharadkova@outlook.com>
2022-06-29 14:21:02 +0200

    Add project files.

---
+++ APItest/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APItest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APItest.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
